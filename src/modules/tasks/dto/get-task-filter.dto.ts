deeaa34e5c074520b6b3b903e3dc4e05fbcc11fe
David Espinoza <deerhou@gmail.com>
2022-06-24 21:45:06 -0500

    optional in gettaskfilter

--- src/modules/tasks/dto/get-task-filter.dto.ts
+++ src/modules/tasks/dto/get-task-filter.dto.ts
@@ -1,6 +1,11 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTaskFilter {
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status: TaskStatus;
+  @IsString()
+  @IsOptional()
   filter: string;
 }
