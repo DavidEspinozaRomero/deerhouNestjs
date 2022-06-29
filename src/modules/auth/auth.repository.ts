44d604edca42019858a13812218df72a79b64b28
David Espinoza <deerhou@gmail.com>
2022-06-28 20:25:21 -0500

    npm i bcrypt

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from "bcrypt";
 @Injectable()
 export class AuthRepository {
   constructor(private dataSource: DataSource) {}
@@ -11,6 +11,9 @@ export class AuthRepository {
   api = this.dataSource.getRepository(User);
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    
+    const salt = await bcrypt.genSalt()
+    
     const { username, password } = authCredentialsDto;
     const user = this.api.create({ username, password });
     try {
